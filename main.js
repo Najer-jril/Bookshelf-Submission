05e6b202750ed19432bc1e675ad8a8dd48b06311
Najer-jril <mnazrililham55@gmail.com>
2024-06-19 10:01:50 +0900

    adding some fiture -js

---
+++ main.js
@@ -0,0 +1,14 @@
+//back to top button
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Tampilkan tombol ketika pengguna menggulir ke bawah
+window.onscroll = function() {
+    let backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
